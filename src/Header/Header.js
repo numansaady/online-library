bbe5213ae3334723d23ea3bf07ce4974f486a3d3
numansaady <numansaady@gmail.com>
2022-03-26 02:19:37 +0600

    Header added

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Boi Ghor</h1>
+        </div>
+    );
+};
+
+export default Header;
